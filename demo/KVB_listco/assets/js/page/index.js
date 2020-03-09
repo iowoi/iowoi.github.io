2d651a0d8e2dd8d1080bd74c8798746f063e1225
Martina <iowoiy.yo@gmail.com>
2020-03-09 17:29:25 +0800

    add KVBLISTCO

---
+++ demo/KVB_listco/assets/js/page/index.js
@@ -0,0 +1,2 @@
+var slider;$(document).ready(function(){slider=$("#indexBanner").bxSlider({mode:"vertical",auto:!0,speed:800,pause:3e3,adaptiveHeight:!1,controls:!1})});
+/*! kvb 最后修改于： 2018-01-22 */
