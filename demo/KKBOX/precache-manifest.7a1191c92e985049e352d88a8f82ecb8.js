b4e5700374bfd6f96f6bdbd0545c483349f133b3
Martina <iowoiy.yo@gmail.com>
2020-08-17 17:15:19 +0800

    add: kkbox api test

---
+++ demo/KKBOX/precache-manifest.7a1191c92e985049e352d88a8f82ecb8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af2bfdef7ec476707a7280302d55805c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61b15fbe113307b9d03e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2f2df01603d1c377a080",
+    "url": "/static/js/2.d0963b22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d0963b22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61b15fbe113307b9d03e",
+    "url": "/static/js/main.bf51df10.chunk.js"
+  },
+  {
+    "revision": "0ca8ba192678f0d2ffb4",
+    "url": "/static/js/runtime-main.251145d4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
